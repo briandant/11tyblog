1f001d9e3d7bc4f4604a3cc12a6fc4000d50bff1
Brian Dant <brian.dant@hey.com>
2024-10-26 22:41:59 -0400

    add prism.js for code highlighting

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,8 @@
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("output.css");
+  eleventyConfig.addPlugin(syntaxHighlight);
   // eleventyConfig.addLayoutAlias("note", "layouts/note.liquid");
   // eleventyConfig.addLayoutAlias("link", "layouts/link.liquid");
 };
