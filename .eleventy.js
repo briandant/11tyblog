f7ca64b6e396d0c9da94117e32e19795eca71cf6
Brian Dant <brian.dant@hey.com>
2024-10-26 20:21:57 -0400

    don't map layouts in config
    planning to fix this via #1

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,5 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("output.css");
+  // eleventyConfig.addLayoutAlias("note", "layouts/note.liquid");
+  // eleventyConfig.addLayoutAlias("link", "layouts/link.liquid");
 };
